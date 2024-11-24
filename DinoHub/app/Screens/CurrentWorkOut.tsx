06d37144598118b2856da370f56692b3e077386b
Y-Alomar <113862815+Y-Alomar@users.noreply.github.com>
2024-11-24 08:19:19 -0700

    CurrentWorkout Page is done

--- DinoHub/app/Screens/CurrentWorkOut.tsx
+++ DinoHub/app/Screens/CurrentWorkOut.tsx
@@ -6,12 +6,36 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import NavBar from './NavBar';
 
 
-import AddButton from '../assets/CurrentWorkOutAssests/AddButton.svg'
+import AddButton from '../assets/CurrentWorkOutAssests/AddButton.svg';
+import Play from '../assets/CurrentWorkOutAssests/Play.svg';
+import Edit from '../assets/CurrentWorkOutAssests/Edit.svg';
+import Remove from '../assets/CurrentWorkOutAssests/Minus.svg';
+
+
 
 type Props = NativeStackScreenProps<RootStackParamList,'Home'>
 const { height, width } = Dimensions.get('window');
 
+
+
+
+
 export default function CurrentWorkOut({navigation}:Props){
+    
+    const data = [
+        {id:1,name:'Temp Template 1'},
+        {id:2,name:'Temp Template 2'},
+        {id:3,name:'Temp Template 3'},
+        {id:4,name:'Temp Template 4'},
+        {id:5,name:'Temp Template 5'},
+        {id:6,name:'Temp Template 6'},
+        {id:7,name:'Temp Template 7'},
+        // {id:8,name:'Temp Template 8'},
+        // {id:9,name:'Temp Template 9'},
+
+    ];
+
+
     return(
 
             <View style={styles.container}>
@@ -19,56 +43,71 @@ export default function CurrentWorkOut({navigation}:Props){
 
                 <Header/>
             
-                <SafeAreaView style={styles.InnerNav}>
-                    <TouchableOpacity style={styles.InnerNavBtn} >
-                        <Text style={styles.InnerNavBtnText}>Templates</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.InnerNavBtn}>
-                        <Text style={styles.InnerNavBtnText}>History</Text>
-                    </TouchableOpacity>
 
-                </SafeAreaView>
+                <View style={styles.Main}>
 
-                <TouchableOpacity style={styles.AddBtnBox}>
-                    <AddButton/>
-                </TouchableOpacity>
 
-                <View>
-                    {/* Will need to be a mapping thing */} 
+                    <View style={styles.InnerNav}>
+
+                        <TouchableOpacity style={styles.InnerNavBtn} >
+                            <Text style={styles.InnerNavBtnText} >Templates</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.InnerNavBtn} >
+                            <Text style={styles.InnerNavBtnText} >History</Text>
+                        </TouchableOpacity>
+
+
+
+                    </View>
                     
-                    <View>
-                        <Text>1</Text>
-                    </View>
 
 
                     <View>
-                        <Text>2</Text>
-                    </View>
-
-                    <View>
-                        <Text>3</Text>
-                    </View>
-
-
-                    <View>
-                        <Text>4</Text>
+                        <TouchableOpacity style={styles.AddBtnBox}>
+                            <AddButton/>
+                        </TouchableOpacity>
                     </View>
 
 
-                    <View>
-                        <Text>5</Text>
+
+                    
+
+
+                    <View style={styles.Content}>
+                        {
+                    
+                            data.map((item)=>(
+                                <View style={styles.ContentBox} key={item.id}>
+                                    <Text>{item.name}</Text>
+                                    
+                                    
+                                    <View style={styles.Buttons}>
+                                        <TouchableOpacity style={styles.play}>
+                                            <Play />
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={styles.edit}>
+                                            <Edit/>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity>
+                                            <Remove/>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            ))
+                    
+                    
+                    
+                    }
                     </View>
 
 
-                    <View>
-                        <Text>6</Text>
-                    </View>
-
-                    <View>
-                        <Text>7</Text>
-                    </View>
 
                 </View>
+
+                
+
+                
 
                 <NavBar/>
 
@@ -90,16 +129,29 @@ const styles = StyleSheet.create({
     container:{
         backgroundColor:'white',
         display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
         flex:1
+    },
+
+
+    Main:{
+        display:'flex',
+        // flexDirection:'column',
+        // marginTop:-116,
+        justifyContent:'space-between',
+        height:height*(670/851)
     },
 
 
     InnerNav:{
         display:'flex',
         flexDirection:'row',
-        marginBottom:23,
-        justifyContent:'space-evenly'
+        justifyContent:'space-evenly',
+        height:height*(74/851),
+        width:width
     },
+
 
 
     InnerNavBtn:{
@@ -124,6 +176,57 @@ const styles = StyleSheet.create({
         display:'flex',
         marginLeft:width*0.8409,
         justifyContent:'flex-end',
+        // marginBottom:height*(300/851)
+    },
+
+
+    Content:{
+        display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginBottom:height*(60/851),
+        height:height*(450/851),
+        width:width
+    },
+
+    ContentBox:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        backgroundColor:'#F2F4FB',
+        width:width*(358/396),
+        height:height*(38/851),
+        marginBottom:height*(30/851),
+        borderColor:'black',
+        borderWidth:1
+    },
+
+    Buttons:{
+        // backgroundColor:'yellow',
+        paddingLeft:50,
+        paddingRight:80,
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        alignItems:'center',
+        // marginRight:100
+    },
+
+    play:{
+        display:'flex',
+        alignItems:'center',
+        borderRightColor:'black',
+        borderRightWidth:1,
+        marginRight:22,
+        // height:height*(38/851)
+
+    },
+
+
+    edit:{
+        marginLeft:22,
+        marginRight:35
     }
 
 
