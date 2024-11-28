09ed8018219b19d0a1179215cdb7f30b58036aec
alejandrocqvd <alejandrocqvd@gmail.com>
2024-11-27 19:37:31 -0700

    search food page

--- DinoHub/app/index.tsx
+++ DinoHub/app/index.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './RootStackParamList';
 import CurrentWorkOut from './Screens/CurrentWorkOut';
 import Nutrition from './Screens/Nutrition';
+import SearchFood from './Screens/SearchFood';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -10,12 +11,13 @@ export default function App() {
   return (
     <Stack.Navigator
       screenOptions={{
-        animation: "none", // Disable animation
-        headerShown: false, // Optional: Disable the header
+        animation: "none",
+        headerShown: false,
       }}
     >
       <Stack.Screen name="Home" component={CurrentWorkOut} />
       <Stack.Screen name="Nutrition" component={Nutrition} />
+      <Stack.Screen name="SearchFood" component={SearchFood} />
     </Stack.Navigator>
   );
 }
